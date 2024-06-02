bc338690bac9ea09421fa0e9dab44d5428d131ab
Matthias Mulugeta <matthias22m@gmail.com>
2024-06-02 09:06:28 +0300

    register created models

---
+++ remote_api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+admin.site.register(models.Team)
+admin.site.register(models.Project)
+admin.site.register(models.Task)
+admin.site.register(models.Feedback)
